0a7419726095f2106dc7c16494df0c0a021aeb61
brunda933 <pes1ug23cs151@pesu.pes.edu>
2026-03-30 01:57:43 +0530

    Add AuctionType enum for supported auction categories

---
+++ Backend/src/main/java/com/auctionsystem/model/AuctionType.java
@@ -0,0 +1,18 @@
+// @owner Brunda
+// @feature Auction creation and item browsing
+package com.auctionsystem.model;
+
+/**
+ * @author Brunda
+ * @description Defines the supported auction categories
+ * @pattern Factory
+ * @solid OCP
+ */
+public enum AuctionType {
+    CRICKET,
+    KABADDI,
+    ANTIQUES,
+    REAL_ESTATE,
+    ART,
+    GENERAL
+}
